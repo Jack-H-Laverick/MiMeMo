b8cbd76dc60abfdab3a06ccfeb077a862f8dabfc
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-03-17 14:38:58 +0000

    Update GFW extractor to use SF objects.

--- R/Global Fishing Watch functions.R
+++ R/Global Fishing Watch functions.R
@@ -3,33 +3,24 @@
 
 #### Global Fishing Watch Data Extraction ####
 
-#' Extract the values from a grid under transects along the external boundaries of the model domain
+#' Import Fishing Effort Within Specified Areas
 #'
-#' This function reads in a datafile and attaches the values needed to transects.
-#' It uses a precalculated set of indices of where transects intersect the grid for speed.
 #'
-#' @param Depth The depth layer to extract data from. Either "S" or "D"
-#' @param Data The data object as provided by Sample_OOB
-#' @param variables The variables to extract, provided by Sample_OOB
-#' @return The function returns a dataframe of transects and their average DIN, chlorophyll, temperature, and salinity values by depth.
+#'
+#' @param file The full name to a .csv file containing Global Fishing Watch data.
+#' @param area A Simple Feature object representing the areas to return fishing activity in.
+#' @return A dataframe of cells from a 0.01 degree grid which contain fishing activity,
+#' labelled by the FAO region they fall in.
 #' @family Global Fishing Watch functions
 #' @export
-Arctic_boats <- function (Data)                {
-  Fish <- Data %>%                                                            # Take first csv file as a test
-    mutate(Date = date) %>%                                            # We need Lon-lat to be the first two columns so move date column
-    mutate(date = lon_bin/100, lat_bin = lat_bin/100)                 # Move Coordinate columns, and divide by 100 because raw data misses decimal point
+get_cropped_fishing <- function (file, area) {
 
-  colnames(Fish)[2] <- "Latitude"                                             # Change column names for GIS functions
-  colnames(Fish)[1] <- "Longitude"
-
-  coordinates(Fish) <- ~ Longitude + Latitude                                 # Converts to a shapefile
-
-  proj4string(Fish) <- proj4string(FAO_arctic)                                # Set the projection of the points to match the polygons
-
-  # Filter the data to rows where polygons lie over the fishing data points
-  inside.Arctic <- Fish[!is.na(over(Fish, as(FAO_arctic, "SpatialPolygons"))),] %>%
-    fortify(inside.Arctic)                                    # Coerce for ggplot
-  return(inside.Arctic) }     # Function to clip boat pings to the FAO regions of interest
+  Fish <- utils::read.csv(file, header = TRUE) %>%                           # Take first csv file as a test
+    dplyr::mutate(Longitude = lon_bin/100, Latitude = lat_bin/100) %>%       # Move Coordinate columns, and divide by 100 because raw data misses decimal point
+    sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326) %>%
+    sf::st_join(area) %>%
+    tidyr::drop_na()
+}
 
 #' Extract the values from a grid under transects along the external boundaries of the model domain
 #'
