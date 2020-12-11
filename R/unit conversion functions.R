a01482c62762aa447adac1f5fcc2d629d7b68ace
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-12-11 18:21:06 +0000

    Added shortwave radiation to einsteins conversion.

--- R/unit conversion functions.R
+++ R/unit conversion functions.R
@@ -4,9 +4,16 @@
 #' These functions are named to make it intuitive to switch between scales, with pairs existing for
 #' different directions of conversion.
 #'
+#' @details
 #' Though some of these functions do the same thing numerically, having multiple names which match the desired
 #' conversion should reduce friction when developing code. This should also help avoid typos when it comes to
 #' 0s, and also stop you needing to constantly look up conversion factors!
+#'
+#' The `shortwave_to_einsteins` function is not exact and is based on a couple of assumptions to get from watts
+#' to einsteins per m^2 per day. It expects an input of total Watts per day per m^2.
+#' \itemize{
+#'  \item{The proportion of PAR from shortwave downward radiation is 0.43.}
+#'  \item{The energy content of a mole of photons is 217.503 kJ, taken from midband radiation (550nm).}}
 #'
 #' @param data A numeric vector.
 #' @return A numeric vector following unit conversion.
@@ -47,3 +54,21 @@ sec_to_day <- function(data) data / 86400
 #' @rdname Convert-units
 #' @export
 day_to_sec <- function(data) data * 86400
+
+#' @rdname Convert-units
+#' @export
+shortwave_to_einstein <- function(data) {
+
+  # 1 watt is 1 joule per second
+  MJ <- (data * 86400)/1e6               # Watts Integrated per day to MegaJoules per day
+  MJ_PAR  <- MJ * 0.43                    # PAR fraction of shortwave radiation
+  einsteins <- MJ_PAR/(217.503/1000)      # 217 kJ per mol, so convert kJ to MJ and divide
+
+  ## So we scaled shortwave radiation by the proportion as PAR and by the ratio
+  ## of moles to energy for mid-band photons (550nm)
+  ## This is not exact. 0.43 is a rule of thumb
+  ## Also the energy content of a mole of photons is an approximation as it varies
+  ## with latitude and across wavelengths
+
+}
+
