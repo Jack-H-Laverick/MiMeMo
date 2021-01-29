64fa2f95b63cf5829ce144c825cbb90e11da61a1
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-29 16:43:15 +0000

    feat: rename files and functions for consistency, and added NM_boundary_summary fore more efficient perimeter sampling.

--- R/NM functions.R
+++ R/NM functions.R
@@ -1,5 +1,5 @@
 
-#### Functions to average volumes once extracted ####
+#### Functions to summarise NEMO-MEDUSA model outputs ####
 
 #' Prepare for Averaging by Decade
 #'
@@ -13,7 +13,7 @@
 decadal <- function(saved) {
 
   import <- readRDS(file = saved) %>%                                   # Read in wide format data file
-    dplyr::select(-c(weights, Bathymetry)) %>%                          
+    dplyr::select(-c(weights, Bathymetry)) %>%
     dplyr::rename(Decade = Year)
 
   stringr::str_sub(import$Decade, -1, -1) <- "0"                        # Overwite the 4th digit with a 0 to get the decade
@@ -41,13 +41,12 @@ strip_ice <- function(data, dt) {
   if(dt == TRUE) {                                                                  # Run data.table method
     data <- data.table::setDT(data)
 
-    if(data$Depth[1] == "D") data[, c("Ice_conc", "Ice_Thickness", "Snow_Thickness"):=NULL] else data
+    if(data$slab_layer[1] == "D") data[, c("Ice_conc", "Ice_Thickness", "Snow_Thickness"):=NULL] else data
   } else{                                                                           # Run dplyr method
 
-    if(data$Depth[1] == "D") {select(data, -c(starts_with("Ice"), Snow_Thickness))} else data}}
+    if(data$slab_layer[1] == "D") {select(data, -c(starts_with("Ice"), Snow_Thickness))} else data}}
 
-#' Average into Decadal Grids
-#'
+#' Summarise NEMO-MEDUSA Output into Decadal Grids
 #' This function averages cleaned NEMO-MEDUSA monthly summaries into decadal grids.
 #'
 #' The function groups by all spatial variables (Longitude, Latitude, Depth, and Shore zone), and by decade and month.
@@ -59,23 +58,22 @@ strip_ice <- function(data, dt) {
 #' @return A dataframe containing a summarised decade of spatialy resolved NEMO-MEDUSA outputs.
 #' @family NEMO-MEDUSA averages
 #' @export
-summarise_sp <- function(decade, dt) {
+NM_decadal_summary <- function(decade, dt) {
 
 if(dt == TRUE){                                                               # Run data.table method
   # data.table::setDT(decade)                                                 # set as a data.table, not needed if decade is already a data.table
   Averaged <- decade[, lapply(.SD, mean, na.rm = TRUE),                       # Average data columns which aren't groups
-                     by = c("Longitude", "Latitude", "Decade", "Month", "Shore", "Depth")] # Group by pixel and decade
+                     by = c("longitude", "latitude", "Decade", "Month", "Shore", "slab_layer")] # Group by pixel and decade
 } else{                                                                       # Run dplyr method
   Averaged <- decade %>%
-    dplyr::group_by(Longitude, Latitude, Decade, Month, Shore, Depth) %>%     # Group by pixel and decade
+    dplyr::group_by(longitude, latitude, Decade, Month, Shore, slab_layer) %>%# Group by pixel and decade
     dplyr::summarise_all(mean, na.rm = TRUE) %>%                              # Average data columns
     dplyr::ungroup()                                                          # Ungroup
   }
   return(Averaged)
 }
 
-#' Average into Time Series
-#'
+#' Summarise NEMO-MEDUSA Output into Time Series Within Model Compartments
 #' This function averages NEMO-MEDUSA monthly summaries into time series for each model compartment.
 #'
 #' The function groups by model compartment (Depth and Shore zone) and time step (Month and Year).
@@ -85,13 +83,13 @@ if(dt == TRUE){                                                               # 
 #' @return A dataframe containing a mean monthly time series of all target variables in NEMO-MEDUSA outputs.
 #' @family NEMO-MEDUSA averages
 #' @export
-summarise_ts <- function(saved) {
+NM_volume_summary <- function(saved) {
 
   Groups <- readRDS(file = saved) %>%                                          # Read in wide format data file
-    dplyr::filter(!weights < 0) %>%                                            # Drop points on land
-    dplyr::mutate(weights = dplyr::na_if(weights, 0)) %>%                      # Replace 0 weights with NA so vector lengths match for weighted mean
+#    dplyr::filter(!weights < 0) %>%                                            # Drop points on land
+#    dplyr::mutate(weights = dplyr::na_if(weights, 0)) %>%                      # Replace 0 weights with NA so vector lengths match for weighted mean
     tidyr::drop_na(Year, Shore) %>%                                            # Drop points outside of the polygons
-    dplyr::group_by(Shore, Year, Month, Depth)
+    dplyr::group_by(Shore, Year, Month, slab_layer)
 
   Ice <- dplyr::filter(Groups, Ice_pres > 0) %>%                               # Remove ice free pixels before averaging
     dplyr::summarise(Ice_Thickness_avg = mean(Ice_Thickness, na.rm = TRUE),    # Get monthly mean sea ice thickness
@@ -104,12 +102,64 @@ summarise_ts <- function(saved) {
               DIN_avg = stats::weighted.mean(DIN, weights, na.rm = TRUE),
               Detritus_avg = stats::weighted.mean(Detritus, weights, na.rm = TRUE),
               Phytoplankton_avg = stats::weighted.mean(Phytoplankton, weights, na.rm = TRUE),
-              Ice_pres = mean(Ice_pres, na.rm = TRUE),                         # Proprtion of pixels covered by ice
-              Vertical_diffusivity_avg = stats::weighted.mean(Vertical_diffusivity, weights, na.rm = TRUE),
-              Vertical_velocity_avg = stats::weighted.mean(Vertical_velocity, weights, na.rm = TRUE),
+              Ice_pres = mean(Ice_pres, na.rm = TRUE),                         # Proportion of pixels covered by ice
               Meridional_avg = stats::weighted.mean(Meridional, weights, na.rm = TRUE),
               Zonal_avg = stats::weighted.mean(Zonal, weights, na.rm = TRUE)) %>%
       dplyr::left_join(Ice) %>%                                                # Add in ice and snow thicknesses
       dplyr::ungroup()
 
   return(Averaged) }
+
+#' Summarise NEMO-MEDUSA Output into Time Series Along Transects
+#'
+#' This function averages NEMO-MEDUSA monthly summaries into time series for the target boundaries of StrathE2E.
+#'
+#' The function subsets the NEMO-MEDUSA grid according to the transects object provided. Water exchanges between
+#' model compartments are totaled. The boundary conditions of the model domain for variables needed by
+#' StrathE2E are summarised as a flow-weighted mean, applying the flow rate at each transect.
+#'
+#' @param saved A dataframe containing a summarised month from NEMO-MEDUSA model outputs.
+#' @param transects A dataframe containing the labelled transects along the model domain boundaries.
+#' @param vars A character vector containing the column names to be summarised for boundary conditions. Defaults to the tragets for StrathE2E
+#' @return A list containing two summaries.
+#' \itemize{
+#'  \item{Element 1}{A dataframe containing the total water exchanged between model compartments.}
+#'  \item{Element 2}{A dataframe containing the flow-weighted boundary conditions around the model domain.}
+#' @family NEMO-MEDUSA averages
+#' @export
+NM_boundary_summary <- function(saved, transects, vars = c("DIN", "Phytoplankton", "Temperature", "Salinity", "Detritus")) {
+
+  Data <- readRDS(saved) %>%                                                  # Import a NM summary object
+    dplyr::select(-c(Shore, weights))                                         # Drop duplicated columns which vonflict
+  data.table::setDT(Data, key = c("x", "y", "slab_layer"))                    # Convert to a data.table keyed spatially for quick summaries.
+
+  join <- Data[transects] %>%                                                 # 0.5% of the transects don't catch data (NA), it's because the deep offshore layer doesn't have a buffer on the shore side.
+    dplyr::mutate(Flow = ifelse(current == "Zonal", Zonal, Meridional)) %>%   # Grab current perpendicular to the transect
+    dplyr::mutate(Flow = ifelse(Flip == T, -1*Flow, Flow)) %>%                # Correct flow so that + always goes IN to a model box
+    dplyr::mutate(Flow = Flow * weights,                                      # Weight by transect area to get the volume of water
+           Direction = ifelse(Flow > 0, "In", "Out"))                         # Label direction based on flow rate
+
+  ## Summarise water exchanges
+
+  water <- join[, .(Flow = sum(Flow, na.rm = T)),                             # Tally up water movements
+                by = c("Shore", "slab_layer", "Direction",                    # By exchanges we want to keep track of
+                       "Neighbour", "Month", "Year")] %>%
+    tidyr::drop_na()                                                          # The NA transects introduce a dead group, remove.
+
+  ## Summarise boundary conditions
+
+  #*# How do we weight by flow? some are negative
+  boundary <- join[perimeter == T & Direction == "In",                        # For transects which bound the perimeter of the model domain
+                   lapply(.SD, weighted.mean, w = Flow, na.rm = T),           # Flow-weighted mean of target variables
+                   by = c("Shore", "slab_layer", "Neighbour",                 # By groups we want to keep track of
+                          "Month", "Year"),
+                   .SDcols = vars] %>%                                        # Specify target variables
+    tidyr::drop_na() %>%                                                      # The NA transects introduce a dead group, remove.
+    dplyr::mutate(Date = as.Date(paste(15, Month, Year, sep = "/"), format = "%d/%m/%Y"),
+           Compartment = paste(Shore, slab_layer)) %>%
+    tidyr::pivot_longer(eval(vars), names_to = "Variable", values_to = "Measured") # reshape
+
+  result <- list(Flows = water, Boundarys = boundary)                         # Combine both summaries so they can be returned together
+
+  return(result)
+}
